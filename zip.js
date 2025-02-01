8f8658b5e60706ce181f85cfd8536d623b109d80
st3v1sh <st3v1sh@gmail.com>
2025-01-31 20:32:59 -0500

    move zip script on its own, fix zip script so old zip is deleted before making a new zip, add twitch extension helper script

--- zip.js
+++ zip.js
@@ -6,6 +6,15 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Delete old zip.
+import { rm } from 'fs';
+rm(join(__dirname, 'dist', 'ace-attorney-investigations-evidence.zip'), (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+
+// Create new zip.
 const output = createWriteStream(join(__dirname, 'dist', 'ace-attorney-investigations-evidence.zip'));
 const archive = archiver('zip', {
   zlib: { level: 9 }
@@ -16,7 +25,7 @@ output.on('close', function () {
   console.log('archiver has been finalized and the output file descriptor has closed.');
 });
 
-archive.on('error', function(err){
+archive.on('error', function (err) {
   throw err;
 });
 
