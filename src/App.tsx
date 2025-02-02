6ed6f9515ac3631df8ad453000eb7b6f06ce39ca
Steven Yu <sky3947@rit.edu>
2025-02-02 17:35:57 -0500

    add zoom setting

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Evidence, Profile } from "./utils/types";
 import EvidenceContent from "./components/EvidenceContent/EvidenceContent";
 import "./App.css";
 import ProfileContent from "./components/ProfileContent/ProfileContent";
+import SettingsContainer from "./components/SettingsModal/SettingsModal";
 
 const App: React.FC = () => {
   const url =
@@ -13,6 +14,8 @@ const App: React.FC = () => {
   const [profileList, setProfileList] = useState<Profile[]>([]);
 
   const [loading, setLoading] = useState<boolean>(false);
+
+  const [isSettingsVisible, setIsSettingsVisible] = useState<boolean>(false);
 
   interface Error {
     id: number;
@@ -112,19 +115,35 @@ const App: React.FC = () => {
                 <div className="tab-content">Profiles</div>
                 {activeTab !== "Profiles" && <div className="tab-shadow"></div>}
               </button>
-              <div className="refresh-container">
+              <div className="options-container">
                 <button className="refresh" onClick={update}>
-                  <span className={loading ? " spinning" : ""}>
-                    <svg
-                      className="spinner"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 30 30"
-                    >
-                      <path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"></path>
-                    </svg>
-                  </span>
+                  <svg
+                    className={"spinner" + (loading ? " spinning" : "")}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 30 30"
+                    fill="var(--primary-color-text)"
+                  >
+                    <path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"></path>
+                  </svg>
+                </button>
+                <button
+                  className="settings"
+                  onClick={() => setIsSettingsVisible(true)}
+                >
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="var(--primary-color-text)"
+                    width="1.9rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M17.2994 10.4527L19.2267 10.7677C19.3846 10.7935 19.5003 10.9298 19.5 11.0896V12.883C19.5 13.0412 19.3865 13.1768 19.2303 13.2042L17.3004 13.543C17.1885 13.9298 17.0349 14.3022 16.8415 14.6543L17.9823 16.2382C18.0759 16.3679 18.0612 16.5463 17.9483 16.6595L16.6804 17.9283C16.5682 18.0401 16.3921 18.0561 16.2623 17.9645L14.6627 16.8424C14.3099 17.0387 13.9352 17.1952 13.5442 17.3103L13.2034 19.231C13.176 19.3865 13.0406 19.5 12.8825 19.5H11.0888C10.9294 19.5 10.7934 19.3849 10.7676 19.228L10.4493 17.3168C10.059 17.204 9.6823 17.0485 9.32585 16.8525L7.73767 17.9648C7.60821 18.0558 7.43178 18.0401 7.31992 17.9283L6.05198 16.6595C5.93947 16.5463 5.9248 16.3686 6.01741 16.2391L7.13958 14.6697C6.94163 14.3116 6.78444 13.9337 6.67062 13.5414L4.76905 13.2042C4.61349 13.1765 4.5 13.0412 4.5 12.883V11.0896C4.5 10.9304 4.61544 10.7941 4.77263 10.768L6.67421 10.4514C6.78868 10.0582 6.94586 9.68022 7.14316 9.32315L6.0347 7.73739C5.94371 7.60793 5.95937 7.43185 6.07122 7.32L7.33883 6.0525C7.452 5.94 7.62908 5.925 7.7592 6.01793L9.33433 7.14293C9.68817 6.94924 10.0639 6.795 10.4552 6.6825L10.767 4.77359C10.7927 4.61576 10.929 4.5 11.0888 4.5H12.8825C13.041 4.5 13.1763 4.61413 13.2037 4.77L13.5399 6.68935C13.929 6.8025 14.304 6.95837 14.6591 7.15467L16.2385 6.01957C16.3683 5.92598 16.5464 5.94065 16.6595 6.05348L17.9278 7.32098C18.0397 7.43315 18.0553 7.60957 17.9643 7.73902L16.8392 9.34239C17.0323 9.69424 17.1865 10.066 17.2994 10.4527ZM9.71725 12C9.71725 13.2607 10.7393 14.2826 12.0001 14.2826C13.2608 14.2826 14.2829 13.2607 14.2829 12C14.2829 10.7394 13.2608 9.71742 12.0001 9.71742C10.7393 9.71742 9.71725 10.7394 9.71725 12Z"></path>
+                  </svg>
                 </button>
               </div>
+              <SettingsContainer
+                isVisible={isSettingsVisible}
+                setIsVisible={setIsSettingsVisible}
+              />
             </div>
             <div className="content-background paper-background"></div>
             {activeTab === "Evidence" ? (
---
+++ src/components/SettingsModal/SettingsModal.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import "./SettingsModal.css";
+
+interface SettingsContainerProps {
+  isVisible: boolean;
+  setIsVisible: (isVisible: boolean) => void;
+}
+
+const SettingsContainer: React.FC<SettingsContainerProps> = ({
+  isVisible,
+  setIsVisible,
+}) => {
+  const baseSize = 16;
+  const [zoomPercentage, setZoomPercentage] = useState(100);
+
+  const zoomOut = () => {
+    if (zoomPercentage > 80) {
+      setZoomPercentage(zoomPercentage - 20);
+    } else {
+      setZoomPercentage(80);
+    }
+  };
+
+  const zoomIn = () => {
+    if (zoomPercentage < 300) {
+      setZoomPercentage(zoomPercentage + 20);
+    } else {
+      setZoomPercentage(300);
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.style.fontSize = `${
+      (baseSize * zoomPercentage) / 100
+    }px`;
+  }, [zoomPercentage]);
+
+  return (
+    isVisible && (
+      <div className="settings-container">
+        <div className="zoom-options">
+          <div className="zoom-icon">
+            <svg
+              fill="var(--primary-color-text)"
+              version="1.1"
+              id="Ebene_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 64 64"
+            >
+              <g>
+                <path d="M21,42c5.071,0,9.728-1.808,13.361-4.811L38.172,41l-1.586,1.586c-0.781,0.781-0.781,2.047,0,2.828l18,18 C54.977,63.805,55.488,64,56,64s1.023-0.195,1.414-0.586l6-6c0.781-0.781,0.781-2.047,0-2.828l-18-18 c-0.781-0.781-2.047-0.781-2.828,0L41,38.172l-3.811-3.811C40.192,30.728,42,26.071,42,21C42,9.42,32.579,0,21,0S0,9.42,0,21 S9.421,42,21,42z M59.172,56L56,59.171L40.828,44L44,40.829L59.172,56z M21,4c9.374,0,17,7.626,17,17s-7.626,17-17,17 S4,30.374,4,21S11.626,4,21,4z"></path>{" "}
+                <path d="M12,23h7v7c0,1.104,0.896,2,2,2s2-0.896,2-2v-7h7c1.104,0,2-0.896,2-2s-0.896-2-2-2h-7v-7c0-1.104-0.896-2-2-2 s-2,0.896-2,2v7h-7c-1.104,0-2,0.896-2,2S10.896,23,12,23z"></path>{" "}
+              </g>
+            </svg>
+          </div>
+          <div className="zoom-percentage-container">
+            <span className="zoom-percentage">{zoomPercentage}%</span>
+          </div>
+          <button className="zoom-button zoom-out" onClick={zoomOut}>
+            <svg
+              width="1.4rem"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M6 12H18" stroke="var(--primary-color-text)"></path>
+            </svg>
+          </button>
+          <button className="zoom-button zoom-in" onClick={zoomIn}>
+            <svg
+              width="1.4rem"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 6V18" stroke="var(--primary-color-text)"></path>
+              <path d="M6 12H18" stroke="var(--primary-color-text)"></path>
+            </svg>
+          </button>
+        </div>
+        <button className="settings-close" onClick={() => setIsVisible(false)}>
+          <svg
+            width="2rem"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 7L15 12L10 17"
+              stroke="var(--primary-color-text)"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    )
+  );
+};
+
+export default SettingsContainer;
